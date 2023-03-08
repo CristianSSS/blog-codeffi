60542545ae9ebb1e40dcd86a2896c7f5e9a99620
Cris <cristian.salgado4756@gmail.com>
2023-03-08 16:59:11 -0300

    cambios theme

--- js/switch-theme.js
+++ js/switch-theme.js
@@ -1,15 +1,19 @@
 const switchInput=document.getElementById("select-theme");
 const html=document.querySelector("html")
 const icon=document.getElementById("icon-theme");
-
+const header=document.querySelector("header");
 switchInput.addEventListener("change",(e)=>{
 
     if(e.target.checked){
         html.setAttribute("data-theme","dark");
         icon.setAttribute("src","./assets/sol.png");
+        icon.style.filter="invert(1)";
+        header.style.backgroundColor="#11191F";
     }else{
         html.setAttribute("data-theme","light");
         icon.setAttribute("src","./assets/creciente.png");
+        icon.style.filter="invert(0)";
+        header.style.backgroundColor="#FFF";
     }
     
 });
