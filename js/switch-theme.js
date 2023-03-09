220eeac2eb4d3495629029e72e6b98a8c2673cdd
CristianSSS <cristian.salgado4756@gmail.com>
2023-03-08 23:05:49 -0300

    cambios

--- js/switch-theme.js
+++ js/switch-theme.js
@@ -2,18 +2,38 @@ const switchInput=document.getElementById("select-theme");
 const html=document.querySelector("html")
 const icon=document.getElementById("icon-theme");
 const header=document.querySelector("header");
+const logo=document.getElementById("logo");
+
+if(html.getAttribute("data-theme")=="light"){
+
+    logo.style.filter="invert(1)";
+    icon.setAttribute("src","./assets/creciente.png");
+    icon.style.filter="invert(0)";
+    header.style.backgroundColor="#FFF";
+    
+
+}else if(html.getAttribute("data-theme")=="dark"){
+    logo.style.filter="invert(0)";
+    header.style.backgroundColor="#11191F";
+    icon.style.filter="invert(1)";
+    icon.setAttribute("src","./assets/sol.png");
+}
+
 switchInput.addEventListener("change",(e)=>{
+    
 
     if(e.target.checked){
         html.setAttribute("data-theme","dark");
         icon.setAttribute("src","./assets/sol.png");
         icon.style.filter="invert(1)";
         header.style.backgroundColor="#11191F";
+        logo.style.filter="invert(0)";
     }else{
         html.setAttribute("data-theme","light");
         icon.setAttribute("src","./assets/creciente.png");
         icon.style.filter="invert(0)";
         header.style.backgroundColor="#FFF";
+        logo.style.filter="invert(1)";
     }
     
 });
